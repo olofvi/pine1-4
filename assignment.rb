6c81f9e17c5f3414ca6aa84a86cd38b910eeac3b
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-04 12:43:16 +0200

    reaasign pr2

--- assignment.rb
+++ assignment.rb
@@ -1,4 +1,4 @@
-composer = 'Mozars'
-puts composer + ' was "da bomb", in his day.'
-composer = 'Beethoven'
-puts 'But I prefer' + composer + ', personally.'
+var = 'just another ' + 'string'
+puts var 
+var = 5* (1+2)
+puts var
